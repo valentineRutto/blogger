cb41ff6008e10959c1d5f5556a7c605dfae2bd79
valentinerutto <rutto28@gmail.com>
2015-08-27 11:45:59 +0300

    added delete link to show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+      include ArticlesHelper
     def index
   @articles = Article.all
 end
@@ -9,11 +10,11 @@ end
 def new
     @article=Article.new
 end
-    def create
-        @article = Article.new(article_params)
+    
+def create
+  @article = Article.new(article_params)
   @article.save
-redirect_to article_path(@article)
-
+  redirect_to article_path(@article)
 end
 
 
