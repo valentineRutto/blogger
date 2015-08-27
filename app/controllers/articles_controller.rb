18c69156f64f95748fcb614bac1caca66cba30f9
valentinerutto <rutto28@gmail.com>
2015-08-27 18:14:07 +0300

    added flash notice on update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,13 +21,14 @@ end
         @article.destroy
         redirect_to articles_path
     end
+    
     def edit
   @article = Article.find(params[:id])
 end
 def update
   @article = Article.find(params[:id])
   @article.update(article_params)
-
+  flash.notice = "Article '#{@article.title}' Updated!"
   redirect_to article_path(@article)
 end
 
