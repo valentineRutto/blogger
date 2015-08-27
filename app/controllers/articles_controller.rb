1d1993bfdc6d8a7362b52d4de818d6b864711c28
valentinerutto <rutto28@gmail.com>
2015-08-27 12:06:52 +0300

    edit link on show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,8 +14,12 @@ end
 def create
   @article = Article.new(article_params)
   @article.save
-  redirect_to article_path(@article)
+  redirect_to articles_path(@article)
 end
-
+    def destroy
+        @article=Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
 
 end
