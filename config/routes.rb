08ea354d0108901791bcd036b48f81874db7ba8b
valentinerutto <rutto28@gmail.com>
2015-09-02 12:26:27 +0300

    listing tags on article page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-    resources :articles do
-          resources :comments
-    end
     root to: 'articles#index'
-
+    resources :articles do
+   resources :comments
+    end
+  resources :tags
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
